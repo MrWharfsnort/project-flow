6b095ba191a0791db6ea49fd7b47076f639d6b24
foxinatardis <abellive@me.com>
2016-10-20 18:56:57 -0600

    fixed timer bug, added bootstrap, added autoBreak

--- public/main.js
+++ public/main.js
@@ -24,6 +24,7 @@ $(document).ready(function () {
 		var name = $("#name").val(); //get username input
 		var pw = $("#newPassword").val(); //get password input
 		var email = $("#newEmail").val(); //get email input
+		console.log(name, pw, email);
 		$.post("/api/register", { //post to the register api
 			name: name,
 			password: pw,
@@ -40,12 +41,27 @@ $(document).ready(function () {
 	$("#startTimer").click(function (evt) {
 		startTime();  //todo: write startTime in timer.js
 		$button = $(evt.target);
-		$button.text("Finish");
-		$button.attr("id", "endTimer");
+		$button.css("display", "none");
+		$("#endTimer").css("display", "inline");
 		$("#endTimer").click(endTime);
 	});
 
 
 	$("#endBreak").click(endBreak);
+
+	$("#yesBreak").click(startBreak);
+
+	$("#noBreak").click(function() {
+		$("#autoBreak").css("display", "none");
+		$("#timer").css("display", "block");
+		setAutoBreak();
+	});
+
+
+
+
+
+
+
 
 });
--- public/timer.js
+++ public/timer.js
@@ -1,18 +1,26 @@
 var timeStamps = [];
 var inputObjects = [];
 var totalTime = 0;
+var breakTime = 0;
+var workTime = 0;
+var breakCount = 0;
 var startDate;
+var t;
+
 
 function startTime() {
 	timeStamps.push(Date.now());
 	startDate = new Date();
 	$("#takeBreak").click(startBreak);
+	setAutoBreak();
 }
 
 
 function startBreak() {
 	timeStamps.push(Date.now());
+	clearTimeout(t);
 	$("#timer").css("display", "none");
+	$("#autoBreak").css("display", "none");
 	$("#break").css("display", "block");
 }
 
@@ -21,13 +29,44 @@ function endBreak() {
 	timeStamps.push(Date.now());
 	$("#break").css("display", "none");
 	$("#timer").css("display", "block");
+	setAutoBreak();
+}
+
+function setAutoBreak() {
+	t = setTimeout(autoBreak, 5000); //todo replace timeout length with actual working value
 }
 
 function endTime() {
+	console.log(timeStamps);
 	timeStamps.push(Date.now());
+	console.log(timeStamps);
+	clearTimeout(t);
 	totalTime = timeStamps[timeStamps.length - 1] - timeStamps[0];
 	totalTime = Math.floor(totalTime/1000);
+	if(timeStamps.length > 2) {
+		for (var i = 1; i < timeStamps.length -1; i +=2) {
+			var tempTime = timeStamps[i + 1] - timeStamps[i];
+			breakTime += tempTime;
+			breakCount++;
+		}
+	}
+	breakTime = Math.floor(breakTime/1000);
+	workTime = totalTime - breakTime;
 	$("#timer").css("display", "none");
 	$("#finished").css("display", "block");
-	console.log(totalTime);
+	console.log(startDate);
+	console.log("Total chunk time: " + totalTime);
+	console.log("Total break time: " + breakTime);
+	console.log("Number of breaks taken: " + breakCount);
+	console.log("Time spent actually working: " + workTime);
 }
+
+function autoBreak() {
+	$("#timer").css("display", "none");
+	$("#autoBreak").css("display", "block");
+}
+
+
+
+
+
