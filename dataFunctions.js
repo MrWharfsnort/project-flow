f8a8f6d8dfc09544d3befeb729847cb931fa2871
foxinatardis <abellive@me.com>
2016-10-22 16:10:49 -0600

    got flow rating working on each interval as created.

--- dataFunctions.js
+++ dataFunctions.js
@@ -4,11 +4,11 @@ module.exports = function dataFunctions(mongoose, User, Chunk, Interval) {
    this.getChunkHistory = function(user, days, cb) {
       User.find({email: user}, {chunks: 1, _id: 0}, (err, data) => {
          if (err) { return console.log('error getting chunk history', err); }
-
+         // console.log("from dataFunctions line 7: " + data);
          var chunkIds = data[0].chunks.slice(data[0].chunks.length - days);
 
          Chunk.find({_id: { $in: chunkIds}}, (err, chunks) => {
-            console.log("datafunction: " + chunks);
+            // console.log("datafunction: " + chunks);
             cb(chunks);
          });
       });
--- server.js
+++ server.js
@@ -16,191 +16,214 @@ var User = require('./UserSchema.js')(mongoose, Chunk);
 var dataFunctionsConstructor = require('./dataFunctions.js');
 var dataFunctions = new dataFunctionsConstructor(mongoose, User, Chunk, Interval);
 
-console.log('hello from datafunctions call in server.js', dataFunctions.getChunkHistory('123', 2, console.log));
-
 // basic config for body-parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // basic config for express-session
 app.use(session({
-   secret: 'hghvjkiouyutfghjhjgtyr6t78yiujknliuytre456rtfghiuhgftr5',
-   resave: false,
-   saveUninitialized: false
+	secret: 'hghvjkiouyutfghjhjgtyr6t78yiujknliuytre456rtfghiuhgftr5',
+	resave: false,
+	saveUninitialized: false
 }));
 
 
 app.get('/', (req, res) => {
-   if (!req.session.email) {
-      res.sendFile(__dirname + '/public/login.html');
-      return;
-   }
-   res.sendFile(__dirname + '/public/index.html');
+	if (!req.session.email) {
+		res.sendFile(__dirname + '/public/login.html');
+		return;
+	}
+	res.sendFile(__dirname + '/public/index.html');
 });
 
 
 app.post('/api/login', (req, res) => {
-   if (!req.body.email || !req.body.password) {
-      res.status(401);
-      console.info('Invalid Login', req.body.email);
-      res.send({status: 'error', message: 'user/pass not entered'});
-      return;
-   }
+	if (!req.body.email || !req.body.password) {
+		res.status(401);
+		console.info('Invalid Login', req.body.email);
+		res.send({status: 'error', message: 'user/pass not entered'});
+		return;
+	}
 
-   User.find({email: req.body.email}, (err, user) => {
-      if (user.length === 0) {
-         res.status(401);
-         res.send({status: 'invalid', message: 'invalid username/passord'});
-      } else if (user[0].password !== req.body.password) {
-         res.status(401);
-         res.send({status: 'invalid', message: 'invalid username/password'});
-      } else {
-         req.session.email = user[0].email;
-         res.send({status:"success"});
-      }
-   });
+	User.find({email: req.body.email}, (err, user) => {
+		if (user.length === 0) {
+			res.status(401);
+			res.send({status: 'invalid', message: 'invalid username/passord'});
+		} else if (user[0].password !== req.body.password) {
+			res.status(401);
+			res.send({status: 'invalid', message: 'invalid username/password'});
+		} else {
+			req.session.email = user[0].email;
+			res.send({status:"success"});
+		}
+	});
 });
 
 app.post('/api/logout', (req, res) => {
-   delete req.session.email;
-   res.send({status: 'logout', message: 'succesfully logged out'});
+	delete req.session.email;
+	res.send({status: 'logout', message: 'succesfully logged out'});
 });
 
 app.post('/api/register', (req, res) => {
-   // find this email in the database and see if it already exists
-   User.find({email: req.body.email}, (err, data) => {
-      if (data.length === 0) {      // if the user doesn't exist
-         var newUser = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-         });
+	// find this email in the database and see if it already exists
+	User.find({email: req.body.email}, (err, data) => {
+		if (data.length === 0) {      // if the user doesn't exist
+			var newUser = new User({
+				name: req.body.name,
+				email: req.body.email,
+				password: req.body.password
+			});
 
-         newUser.save((err) => { // save the newUser object to the database
-            if (err) {
-               res.status(500);
-               console.error(err);
-               res.send({status: 'Error', message: 'unable to register user:' + err});
-            }
-            res.send({status: 'success', message: 'user added successfully'});
-         });
-      } else if (err) { // send back an error if there's a problem
-         res.status(500);
-         console.error(err);
-         res.send({status: 'Error', message: 'server error: ' + err});
-      } else {
-         res.send({status: 'Error', message: 'user already exists'}); // otherwise the user already exists
-      }
-   });
+			newUser.save((err) => { // save the newUser object to the database
+				if (err) {
+					res.status(500);
+					console.error(err);
+					res.send({status: 'Error', message: 'unable to register user:' + err});
+				}
+				res.send({status: 'success', message: 'user added successfully'});
+			});
+		} else if (err) { // send back an error if there's a problem
+			res.status(500);
+			console.error(err);
+			res.send({status: 'Error', message: 'server error: ' + err});
+		} else {
+			res.send({status: 'Error', message: 'user already exists'}); // otherwise the user already exists
+		}
+	});
 });
 
 
 app.get('/api/chunk/history', (req, res) => {
-   var user = User.find({email: req.body.email});
+	var user = User.find({email: req.body.email});
 
-   for (var chunk in user.chunks) {
+	for (var chunk in user.chunks) {
 
-   }
-   res.send({message: 'return past 7 days'});
+	}
+	res.send({message: 'return past 7 days'});
 });
 
 app.post('/api/chunk/new', (req, res) => {
-   var chunk = new Chunk();
+	var chunk = new Chunk();
 
-   chunk.save((err) => {
-      if (err) {
-         console.log(err);
-         res.status(500);
-         res.send({message: "error saving chunk"});
-         return;
-      }
+	chunk.save((err) => {
+		if (err) {
+			console.log(err);
+			res.status(500);
+			res.send({message: "error saving chunk"});
+			return;
+		}
 
-      var chunkId = chunk._id;
+		var chunkId = chunk._id;
 
-      User.findOneAndUpdate(
-         {email: req.session.email},
-         {$push: {chunks: chunkId}},
-         (err, data) => {
-         if(err) {
-            console.log(err);
-            res.status(500);
-            res.send({message: "error updating user"});
-            return;
-         }
-         res.send({chunkId: chunkId});
-      });
-   });
+		User.findOneAndUpdate(
+			{email: req.session.email},
+			{$push: {chunks: chunkId}},
+			(err, data) => {
+			if(err) {
+				console.log(err);
+				res.status(500);
+				res.send({message: "error updating user"});
+				return;
+			}
+			res.send({chunkId: chunkId});
+		});
+	});
 });
 
 app.post('/api/chunk/interval', (req, res) => {
-   var interval = new Interval({
-      timeFromStart: req.body.timeFromStart,
-      perceivedChallenge: req.body.challenge,
-	   percievedSkill: req.body.skill,
-	   activity: req.body.activity,
-	   caffeine: req.body.caffeine,
-	   snack: req.body.food
-   });
 
-   interval.save((err) => {
-      if (err) {
-         console.log(err);
-         res.status(500);
-         res.send({message: "error saving interval"});
-         return;
-      }
+	dataFunctions.getChunkHistory(req.session.email, 7, function(chunks) {//get history to compare skill and challenge average to current values
+		var flow;
+		var sa = 0;
+		var ca = 0;
+		var count = 0;
+		for(var each of chunks) {
+			if(each.skillAverage){
+				sa += each.skillAverage;
+				ca += each.challengeAverage;
+				count++;
+			}
+		}
+		sa = sa / count; //skill average for past 7 chunks
+		ca = ca / count; //challenge average for past 7 chunks
+		if(sa && ca) {//this tree calculates current flow rating for interval
+			if(req.body.skill < sa && req.body.challenge > ca) {
+				flow = req.body.skill - sa;
+			} else if (req.body.skill > sa && req.body.challenge < ca) {
+				flow = req.body.challenge - ca;
+			} else {
+				flow = (req.body.challenge - ca) + (req.body.skill - sa);
+			}
+		}
+		var interval = new Interval({//create new interval
+			timeFromStart: req.body.timeFromStart,
+			perceivedChallenge: req.body.challenge,
+			percievedSkill: req.body.skill,
+			activity: req.body.activity,
+			caffeine: req.body.caffeine,
+			snack: req.body.food,
+			flow: flow
+		});
 
-      Chunk.findOneAndUpdate(
-         { _id: req.body.chunkId },
-         { 
-            $push: { intervals: interval._id },
-            $inc: {skillTotal: req.body.skill,
-                  challengeTotal: req.body.challenge
-                  }
-         },{new:true},
-         (err, data) => {
-            if (err) {
-               console.log(err);
-               res.status(500);
-               res.send({message: "error updating chunk"});
-               return;
-            }
-            var skillTotal = data.skillTotal;
-            var challengeTotal = data.challengeTotal;
-            var intervalLength = data.intervals.length;
-            Chunk.findOneAndUpdate(
-               {_id: req.body.chunkId},
-               {
-                  skillAverage: skillTotal/intervalLength,
-                  challengeAverage: challengeTotal/intervalLength
-               },
-               (err, data) => {
-                  if(err){
-                     return console.log(err);
-                  }
-                  res.send("success");
-               }
-            );
-         }
-      );
-   });
+		interval.save((err) => {//save interval
+			if (err) {
+				res.status(500);
+				res.send({message: "error saving interval"});
+				return;
+			}
+
+			Chunk.findOneAndUpdate(
+				{ _id: req.body.chunkId },
+				{ 
+					$push: { intervals: interval._id },
+					$inc: {skillTotal: req.body.skill,
+							challengeTotal: req.body.challenge
+							}
+				},{new:true},
+				(err, data) => {
+					if (err) {
+						console.log(err);
+						res.status(500);
+						res.send({message: "error updating chunk"});
+						return;
+					}
+					var skillTotal = data.skillTotal;
+					var challengeTotal = data.challengeTotal;
+					var intervalLength = data.intervals.length;
+					Chunk.findOneAndUpdate(
+						{_id: req.body.chunkId},
+						{
+							skillAverage: skillTotal/intervalLength,
+							challengeAverage: challengeTotal/intervalLength
+						},
+						(err, data) => {
+							if(err){
+								return console.log(err);
+							}
+							res.send("success");
+						}
+					);
+				}
+			);
+		});
+	});
 });
 
 app.post('/api/chunk/done', (req, res) => {
-   Chunk.findOneAndUpdate(
-      {_id: req.body.chunkId}, {
-         timeSpent: req.body.timeSpent,
-         timeOfDay: req.body.timeOfDay,
-         hoursSlept: req.body.sleep,
-         mealsEaten: req.body.meals
-      }, (err, data) => {
-      if (err) {
-         console.log(err);
-         res.status(500);
-         res.send({message: "error updating chunk"});
-      }
-      res.send("success");
-   });
+	Chunk.findOneAndUpdate(
+		{_id: req.body.chunkId}, {
+			timeSpent: req.body.timeSpent,
+			timeOfDay: req.body.timeOfDay,
+			hoursSlept: req.body.sleep,
+			mealsEaten: req.body.meals
+		}, (err, data) => {
+		if (err) {
+			console.log(err);
+			res.status(500);
+			res.send({message: "error updating chunk"});
+		}
+		res.send("success");
+	});
 });
 
 // serve up static content in the public folder
@@ -209,21 +232,21 @@ app.use(express.static('public'));
 
 // 404 error handling
 app.use((req, res, next) => {
-   res.status(404);
-   console.error('404 - ' + req.url);
-   res.send({status: 'Error', message: '404 - File not found'});
+	res.status(404);
+	console.error('404 - ' + req.url);
+	res.send({status: 'Error', message: '404 - File not found'});
 });
 
 // 500 error handling
 app.use((err, req, res, next) => {
-   res.status(500);
-   console.error('Server error: ' + err);
-   res.send({status: 'Error', message: '500 - Server Error'});
+	res.status(500);
+	console.error('Server error: ' + err);
+	res.send({status: 'Error', message: '500 - Server Error'});
 });
 
 // start the server
 app.listen(PORT, () => {
-   console.info('Server started on http://localhost:' + PORT);
+	console.info('Server started on http://localhost:' + PORT);
 });
 
 
